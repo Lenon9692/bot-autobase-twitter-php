1cd328f5e65ade2b597a3cbb5c5012fd36f19f13
Lenon9692 <137380813+Lenon9692@users.noreply.github.com>
2023-06-29 19:29:18 +0700

    perubahan nama
    zxcsd

--- config.php
+++ config.php
@@ -2,18 +2,18 @@
 
 // session_start();
 require "autoload.php";
-define("CONSUMER_KEY", "");
-define("CONSUMER_SECRET", "");
-define("OAUTH_TOKEN", "");
-define("OAUTH_TOKEN_SECRET", "");
+define("CONSUMER_KEY", "bZQdYU4N8aUUWCv29shEG66VY");
+define("CONSUMER_SECRET", "6tKqSD3BCrWsy9b4M5tOHHzuaAFDXw9zLpQDepn0rkq1P7MM7b");
+define("OAUTH_TOKEN", "3624538703-35usmIEkLWQPk7tfuEkAOe1rdru6rYn8gVm3Lfh");
+define("OAUTH_TOKEN_SECRET", "xfbUuRevcDdWFdC8e9kQ9o9gmd7bW0wiKLaVBIa3NZXe7");
 //define("OAUTH_CALLBACK", ""); //callback url, isi jika dibutuhkan. (optional)
 define("WEBHOOK_URL", ""); //https://domain-kamu.com/twitter/webhook.php
 define("ENV_LABEL", ""); //Dev environment label
 
-define("TRIGGER_WORD", "dog");
-define("ADMIN_ID", ["965702083", "", ""]); //User ID Admin (optional)
-define("MINIMAL_FOLLOWERS", "10");
-define("SLEEP_TIME", "15"); //delay ketika post tweet (detik)
+define("TRIGGER_WORD", "boss");
+define("ADMIN_ID", ["1635148260359954432", "", ""]); //User ID Admin (optional)
+define("MINIMAL_FOLLOWERS", "50");
+define("SLEEP_TIME", "300"); //delay ketika post tweet (detik)
 
 define("TRIGGER_OFF", "off"); //tambahkan kata off di bio maka bot akan nonaktif
 
@@ -21,22 +21,22 @@ define("ALERT_NOT_FOLLOW", "[BOT] kamu belum follow base ini");
 define("ALERT_NOT_FOLLBACK", "[BOT] kamu belum difollback, tunggu sesi open follback berikutnya");
 define("ALERT_MINIMAL_FOLLOWERS", "[BOT] followers kamu minimal " . MINIMAL_FOLLOWERS . " untuk ngirim menfess");
 define("ALERT_ERROR_UNSEND", "[BOT] terjadi kegagalan! hubungi admin untuk unsend manual");
-define("ALERT_DOUBLE_UNSEND", "[BOT] kan tadi udah dihapus njing. Ketik /unsend [url_menfess] untuk unsend menfess berbentuk thread");
+define("ALERT_DOUBLE_UNSEND", "[BOT] kamu sudah mengirim menfess sebelumnya. Ketik /unsend [url_menfess] untuk unsend menfess berbentuk thread");
 define("ALERT_NOT_SENDER", "[BOT] lo bukan sendernya");
 define("ALERT_UNSEND", "[BOT] menfess kamu sudah berhasil dihapus! Ketik /unsend [url_menfess] untuk unsend menfess berbentuk thread");
 define("ALERT_OFF", "[BOT] bot sedang off, coba dilain waktu ya");
 define("ALERT_BADWORDS", "[BOT] ada kata yang dilarang");
 define("ALERT_TRIGGER", "[BOT] gunakan kata " . TRIGGER_WORD . " untuk ngirim menfess");
 
-define("WELCOME_MESSAGE", "[BOT] hai anak anjing! kamu sudah difollback! baca rules terlebih dahulu sebelum ngirim menfess yaa");
+define("WELCOME_MESSAGE", "[BOT] hai, kamu sudah difollback! baca rules terlebih dahulu sebelum ngirim menfess yaa");
 define("PROCESSED_MESSAGE", "[BOT] wait! menfess kamu sedang diproses");
 define("DECLINED_MESSAGE", "[BOT] menfess kamu batal dikirim");
 
 define("SUCCESS_ALERT", "[BOT] Menfess kamu berhasil di-Tweet. Ketik /unsend untuk batal mengirim");
-define("QUESTION_QUICKREP", "[BOT] pastikan sudah membaca rules, yakin mau ngirim dog?");
+define("QUESTION_QUICKREP", "[BOT] pastikan sudah membaca rules, yakin mau kirim menfess ini?");
 
 define("OPTION_SATU", trim(html_entity_decode('&#x1f436;')) . " Yakin");
-define('DESCRIPTION_SATU', 'Iya anjing');
+define('DESCRIPTION_SATU', 'Iya');
 
 define("OPTION_DUA", trim(html_entity_decode('&#x1f431;')) . " Gak");
 define("DESCRIPTION_DUA", "Batal");
